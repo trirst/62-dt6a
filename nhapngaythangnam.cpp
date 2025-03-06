84a9b0c7ab3a0e4b7d4dd3801979c74f5da2639e
trirst <ngoviettri0@gmail.com>
2025-03-06 20:42:58 +0700

    Add files via upload

---
+++ nhapngaythangnam.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int ngay, thang, nam;
+ 
+    printf("Nhap ngay: ");
+    scanf("%d", &ngay);
+    printf("Nhap thang: ");
+    scanf("%d", &thang);
+    printf("Nhap nam: ");
+    scanf("%d", &nam);
+    printf("Hom nay la ngay %02d/%02d/%d\n", ngay, thang, nam);
+
+    
+}
+
